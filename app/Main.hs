f15e78767ac2fa522f74f4adf373716fa62f2f33
t4ccer <t4ccer@gmail.com>
2021-03-18 16:45:07 +0000

    Add problem 42

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,7 @@ import           Problems.Problem27
 import           Problems.Problem3
 import           Problems.Problem39
 import           Problems.Problem4
+import           Problems.Problem42
 import           Problems.Problem46
 import           Problems.Problem48
 import           Problems.Problem5
@@ -40,5 +41,6 @@ main = do
   solve "Problem 25" problem25
   solve "Problem 27" problem27
   solve "Problem 39" problem39
+  solve "Problem 42" problem42
   solve "Problem 46" problem46
   solve "Problem 48" problem48
