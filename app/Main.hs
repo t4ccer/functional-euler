c152fe7cd15f578dd20f74bef74cc6d1a2092ba6
t4ccer <t4ccer@gmail.com>
2021-03-21 14:22:27 +0000

    Add problem 35

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,7 @@ import           Problems.Problem2
 import           Problems.Problem25
 import           Problems.Problem27
 import           Problems.Problem3
+import           Problems.Problem35
 import           Problems.Problem39
 import           Problems.Problem4
 import           Problems.Problem42
@@ -43,6 +44,7 @@ main = do
   solve "Problem 16" problem16
   solve "Problem 25" problem25
   solve "Problem 27" problem27
+  solve "Problem 35" problem35
   solve "Problem 39" problem39
   solve "Problem 42" problem42
   solve "Problem 46" problem46
---
+++ src/Problems/Problem35.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE NumericUnderscores #-}
+module Problems.Problem35 where
+
+import           Lib
+
+problem35 :: Int
+problem35 = count isCircularPrime [2..999_999]
+
+isCircularPrime :: Int -> Bool
+isCircularPrime = all (isPrime . read) . rotations . show
+
+rotations :: Eq a => [a] -> [[a]]
+rotations xs = (xs :) $ takeWhile (/=xs) $ tail $ iterate rotate xs
+
+rotate :: [a] -> [a]
+rotate n = last n : init n
